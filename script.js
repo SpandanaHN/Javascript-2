49d21650fa8efe2c233922a59ec34d68f89250dc
SpandanaHN <99424199+SpandanaHN@users.noreply.github.com>
2022-04-15 17:53:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var str="Skill Safari"
+console.log(str)
+console.log(str.length)
+
+console.log(str.toUpperCase(str))
+
+console.log(str.indexOf("a"))
+
+console.log("Captain America:This isn't going to change what happened\nIron man:I don't care he killed my mom")
+
+var weight=50
+var height=2
+const ratio=((weight)/(height*height))
+console.log(ratio)
